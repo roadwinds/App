719a3c7663f722931e3585eb906000b257765811
roadwinds <71748832+roadwinds@users.noreply.github.com>
2022-02-08 15:34:54 +0800

    看护人端更新入一个文件夹下
    登录界面
    初始账号test
    初始密码123456

---
+++ 看护人端/app/src/main/java/com/example/guardian/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.guardian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn1;
+    private EditText ET1,ET2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn1);
+        ET1 = findViewById(R.id.ET1);
+        ET2 = findViewById(R.id.ET2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ET1.getText().toString()=="test"&&ET2.getText().toString()=="123456"){
+                    Toast.makeText(getApplicationContext(),"登录成功",Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(getApplicationContext(),"登录失败",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
